1d164800f2be55d3e847b03d1a859fdc293a6719
Jorge Soria <jorge_soriax@outlook.com>
2023-04-18 22:53:04 -0600

    feat: added image modal for Portfolio section

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -1,18 +1,91 @@
-import { Grid, GridItem, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  VStack,
+  useBreakpointValue,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { LgContainer } from "../Containers";
 import SectionHeader from "../SectionHeader";
+import { RoundedButton } from "../Buttons";
+import CrystalBox from "../CrystalBox";
 
 export default function Portfolio({ data }) {
   const Project = ({ title, cover, alt }) => {
-    return (
-      <Image
-        w="100%"
-        objectFit="cover"
-        src={cover}
-        alt={alt}
-        rounded="2xl"
-      />
-    );
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const showModal = useBreakpointValue({ base: false, lg: true });
+
+    const ImageModal = () => {
+      return (
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          isCentered
+          size={{ base: "full", lg: "4xl" }}
+        >
+          <ModalOverlay />
+          <ModalContent rounded="2xl" bg="altGray.dark">
+            <CrystalBox>
+              <ModalBody p={0} mb={6}>
+                <Image
+                  w="100%"
+                  objectFit="cover"
+                  src={cover}
+                  alt={alt}
+                  rounded="xl"
+                />
+              </ModalBody>
+
+              <ModalFooter p={0}>
+                <HStack justify="space-between" w="full">
+                  <Text>{title}</Text>
+                  <RoundedButton onClick={onClose}>Cerrar</RoundedButton>
+                </HStack>
+              </ModalFooter>
+            </CrystalBox>
+          </ModalContent>
+        </Modal>
+      );
+    };
+
+    if (showModal) {
+      return (
+        <>
+          <Image
+            w="100%"
+            objectFit="cover"
+            src={cover}
+            alt={alt}
+            rounded="2xl"
+            onClick={onOpen}
+            cursor="pointer"
+          />
+          <ImageModal />
+        </>
+      );
+    } else {
+      return (
+        <Image
+          display={{ base: "initial", lg: "none" }}
+          w="100%"
+          objectFit="cover"
+          src={cover}
+          alt={alt}
+          rounded="2xl"
+        />
+      );
+    }
   };
   const ProjectGrid = () => {
     return (

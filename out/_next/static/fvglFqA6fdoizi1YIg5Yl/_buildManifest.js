77b69ed30449449947c2f9c88f6b0d959b6ed1a6
Jorge Soria <jorge_soriax@outlook.com>
2023-06-11 12:43:58 -0600

    feat: added /out folder

---
+++ out/_next/static/fvglFqA6fdoizi1YIg5Yl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/745-42c9a253e5962237.js","static/chunks/pages/index-b486950d14ed33db.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
